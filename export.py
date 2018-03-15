3ab1a987205b0a31fb670bd983f77b93fd5f3bac
Anton Smorodskyi <asmorodskyi@suse.com>
2018-03-15 15:27:16 +0100

    Add logging

--- export.py
+++ export.py
@@ -3,7 +3,10 @@
 import argparse
 import csv
 import requests
+import logging
 
+logging.basicConfig(filename='export.log', level=logging.INFO,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 parser = argparse.ArgumentParser()
 parser.add_argument('--parsefile', required=True)
 parser.add_argument('--targeturl', default='http://fromm.arch.suse.de:8086')
@@ -12,13 +15,17 @@ args = parser.parse_args()
 url = args.targeturl + '/write?db=data'
 HW = 'aribeach-fromm'
 
+logging.info('Starting export. parserfile={0} targeturl={1} HW={2}'
+             .format(args.parsefile, args.targeturl, HW))
+
 with open(args.parsefile, 'rb') as csvfile:
     csvreader = csv.DictReader(csvfile, delimiter=',')
     for row in csvreader:
-        request_string = 'throughput, testcase = {0}, traffic_type = {1}, \
-        packetsize = {2},vswitch = {3},hw = {4} value ={5}'.format(
-            row['type'], row['traffic_type'], row['packet_size'],
-            row['vswitch'], HW, row['throughput_rx_mbps'])
-        print 'Will post {0} to {1} '.format(request_string, url)
+        request_string = 'throughput,testcase={0},traffic_type={1},' \
+            .format(row['type'], row['traffic_type'])
+        request_string += 'packetsize={0},vswitch={1},hw={2} value={3}' \
+            .format(row['packet_size'], row['vswitch'], HW,
+                    row['throughput_rx_mbps'])
+        logging.info('Posting data - {0}'.format(request_string))
         response = requests.post(url, data=request_string)
-        print 'Response from DB: {0}'.format(response)
+        logging.info('Response from DB: {0}'.format(response))
