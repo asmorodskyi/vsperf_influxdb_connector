ddd5fa93eafcaa1f46b661adc240631997b11ed0
Jose Lausch <jalausuch@suse.com>
2018-08-08 11:15:05 +0200

    Push all fields to the DB

--- export.py
+++ export.py
@@ -11,14 +11,53 @@ def parse_csv(parsefile, os_version, os_build, vswitch_version, openqa_url):
     with open(parsefile, 'rb') as csvfile:
         csvreader = csv.DictReader(csvfile, delimiter=',')
         for row in csvreader:
-            request_string = 'throughput,testcase={0},traffic_type={1},' \
-                .format(row['type'], row['traffic_type'])
-            request_string += 'packetsize={0},vswitch={1},' \
-                .format(row['packet_size'], row['vswitch'])
-            request_string += 'os_version={0},os_build={1},vswitch_version={2},'.format(
-                os_version, os_build, vswitch_version)
-            request_string += 'openqa_url={0} value={1}'.format(
-                openqa_url, row['throughput_rx_mbps'])
+            request_string = 'throughput,'
+            request_string += (
+                'tx_frames={0},'
+                'rx_frames={1},'
+                'tx_rate_fps={2},'
+                'throughput_rx_fps={3},'
+                'tx_rate_mbps={4},'
+                'throughput_rx_mbps={5},'
+                'frame_loss_percent={6},'
+                'min_latency_ns={7},'
+                'max_latency_ns={8},'
+                'avg_latency_ns={9},'
+                'type={10},'
+                'packet_size={11},'
+                'id={12},'
+                'deployment={13},'
+                'vswitch={14},'
+                'traffic_type={15},'
+                'test_execution_time={16},').format(
+                row['tx_frames'],
+                row['rx_frames'],
+                row['tx_rate_fps'],
+                row['throughput_rx_fps'],
+                row['tx_rate_mbps'],
+                row['throughput_rx_mbps'],
+                row['frame_loss_percent'],
+                row['min_latency_ns'],
+                row['max_latency_ns'],
+                row['avg_latency_ns'],
+                row['type'],
+                row['packet_size'],
+                row['id'],
+                row['deployment'],
+                row['vswitch'],
+                row['traffic_type'],
+                row['test_execution_time'])
+            request_string += (
+                'os_version={0},'
+                'os_build={1},'
+                'openqa_url={2},'
+                'vswitch_version={3}').format(
+                os_version,
+                os_build,
+                openqa_url,
+                vswitch_version)
+            request_string += ' value={0}'.format(row['throughput_rx_mbps'])
+
             logging.info('Posting data - {0}'.format(request_string))
             response = requests.post(url, data=request_string)
             logging.info('Response from DB: {0}'.format(response.content))
